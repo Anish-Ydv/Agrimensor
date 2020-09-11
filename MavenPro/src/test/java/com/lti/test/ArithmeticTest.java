c8c7b1a0d5837d80c5efd486415a435e858b8e45
'Anish-Ydv' <anish.yadav11@gmail.com>
2020-09-11 21:11:11 +0530

    Eclipse Maven Project

---
+++ MavenPro/src/test/java/com/lti/test/ArithmeticTest.java
@@ -0,0 +1,22 @@
+package com.lti.test;
+
+import com.lti.code.Arithmetic;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArithmeticTest {
+	private Arithmetic arth;
+	
+	@Before
+	public void init() {
+		arth = new Arithmetic();
+	}
+	
+	@Test
+	public void testSum() {
+		assertEquals(10,arth.sum(4, 6));
+	}
+}
